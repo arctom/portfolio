95aff24d4199bf721bfc2a6f3f2fb804e42e1595
Tom Arc <tarc@ndscognitivelabs.com>
2026-05-02 15:59:55 -0600

    first commit

---
+++ components/Footer/index.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Socials from "../Socials";
+import Button from "../Button";
+
+const Footer = ({}) => {
+  return (
+      <div className="mt-5 laptop:mt-40 p-2 laptop:p-0">
+        <div>
+          <h1 className="text-2xl text-bold">Contact.</h1>
+          <div className="mt-10">
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              LET&apos;S WORK
+            </h1>
+            <h1 className="text-3xl tablet:text-6xl laptop:text-6xl laptopl:text-8xl text-bold">
+              TOGETHER
+            </h1>
+            <h3 className="text-bold">+52 (625) 589 8930</h3>
+              <Socials />
+          </div>
+        </div>
+      </div>
+  );
+};
+
+export default Footer;
